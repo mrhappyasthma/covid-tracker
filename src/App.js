ad2f615d4919a8c21af7d8ecfdf1aaac888a0678
Mark Klara <mrhappyasthma@gmail.com>
2020-12-31 18:16:07 -0500

    Add the COVID cards: UI and data fetch.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
+import { fetchData } from './api';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
 class App extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-          <Cards />
+          <Cards data={data} />
           <Chart />
           <CountryPicker />
       </div>
